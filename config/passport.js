53b0c23ff9089942fe7aaeb211dc6c81cba81f6f
Deepanjan Ghosh <deepanjan.ghosh@valuelabs.com>
2018-07-18 21:30:09 +0530

    passport syntax modified

--- config/passport.js
+++ config/passport.js
@@ -5,10 +5,11 @@ const config = require('../config/database');
 
 module.exports = function(passport){
   let opts = {};
-  opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
+  opts.jwtFromRequest = ExtractJwt.fromAuthHeaderWithScheme("jwt")
   opts.secretOrKey = config.secret;
   passport.use(new JwtStrategy(opts, (jwt_payload, done) => {
-    User.getUserById(jwt_payload.data._id, (err, user) => {
+    console.log('jwt_payload._doc._id',jwt_payload);
+    User.getUserById(jwt_payload._id, (err, user) => {
       if(err){
         return done(err, false);
       }
